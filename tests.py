0ffd69d9e1af4afa076826aa181d91b00a62ad96
AlexXG0152 <34213814+AlexXG0152@users.noreply.github.com>
2021-07-28 22:47:33 +0300

    Simple func tests

---
+++ tests.py
@@ -0,0 +1,23 @@
+from unittest.mock import patch
+import unittest
+
+from shortener import linkInput, checkInput, short
+
+class PasswordsTestCase(unittest.TestCase):
+
+    def test_get_input_processed_input_correctly(self):
+        user_input = ["www.google.com/"]
+        expected_input = "www.google.com/"
+        with patch('builtins.input', side_effect=user_input):
+            user_in = linkInput()
+        self.assertEqual(user_in, expected_input)
+    
+    def test_checkInput(startLink):
+        assert checkInput("www.google.com") == "http://www.google.com"
+
+    def test_short(startLink):
+        assert short("www.google.com") == "http://www.bit.ly"[:17]
+
+
+if __name__ == '__main__':
+    unittest.main()
